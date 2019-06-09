9637d4aeca0bddc4faf637f88d0e1296ebc60fb9
LeonidPred <multoakk1@yandex.ru>
2019-06-09 13:29:06 +0300

    git add

---
+++ homeworkCsharp/homeworkCsharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeworkCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo symbol;
+            int space = 0;
+            while (true)
+            {
+             symbol = Console.ReadKey();
+                if(symbol.Key == ConsoleKey.Spacebar)
+                {
+                    ++space;
+                }
+                if (symbol.Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("\nКоличество пробелов: " + space);
+            Console.ReadKey();
+        }
+    }
+}
